473af40cf96615e8d3979a5d358fbc2cae44ebef
Callum White <callumw1991@gmail.com>
2018-04-23 13:05:41 +1000

    clojure.test

--- test/hanging_spandex_problem/core_test.clj
+++ test/hanging_spandex_problem/core_test.clj
@@ -1,11 +1,19 @@
 (ns hanging-spandex-problem.core-test
-  (:use midje.sweet)
+  ;;(:use midje.sweet)
   (:require [qbits.spandex :as spandex]
-            [hanging-spandex-problem.core :refer :all]))
+            [clojure.test :refer :all]
+            [hanging-spandex-problem.core :refer :all]
+            [clojure.java.shell :as shell]))
 
 (def host "http://127.0.0.1:9200")
 (def client (memoize (fn [] (spandex/client {:hosts [host]}))))
 
-(fact "something"
-      (spandex/request (client) {:url "/_cat/indexes" :method :get})
-      1 => 0)
+;; (fact "something"
+;;       (spandex/request (client) {:url "/_cat/indexes" :method :get})
+
+;;       1 => 0)
+
+(deftest hanging-test
+  (testing "hanging test"
+    (spandex/request (client) {:url "/_cat/indexes" :method :get})
+    (is (= 1 0))))
